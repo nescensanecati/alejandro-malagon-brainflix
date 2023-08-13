f1b0e2f825bdc5d1bed8563fd2e32ffd7e6f6367
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-13 10:36:54 -0400

    Updated the Upload Page post
    Updated the code used by the Upload Page to be able to create a new video after clicking the submit page.
    Added a refresh for the cancel function, so the form is cleared.
    Fixed some small styling for the form buttons on the upload page.

--- src/Pages/UploadPage/UploadPage.js
+++ src/Pages/UploadPage/UploadPage.js
@@ -1,44 +1,85 @@
 import './UploadPage.scss'
 import publishIcon from '../../Assets/Images/publish.svg'
-import uploadImg from '../../Assets/Images/Upload-video-preview.jpg'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import axios from 'axios';
+// import UploadPageComponent from '../../Components/UploadPage/UploadPage'
 
 
 function UploadPage() {
-    function handleSubmitClick () {
-        alert ('Video was uploaded')
+    const [uploadTitle, setUploadTitle] = useState('');
+    const [uploadDescription, setuploadDescription] = useState('');
+
+    const handleTitleChange = (e) => {
+        setUploadTitle(e.target.value);
+    };
+
+    const handleDescriptionChange = (e) => {
+        setuploadDescription(e.target.value);
+    };
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+
+        const jsonSendToApi = {
+            "title" : uploadTitle,
+            "description" : uploadDescription
+        }
+        axios.post ('http://localhost:8000/videos', jsonSendToApi)
+        .then(
+            response => {
+                console.log('Video was uploaded with HTTP code: ' + response.status + ', and message:' + response.statusText);
+                alert('Video was uploaded')
+                window.location.replace("/");
+            }
+        )
+        .catch(error => {
+            console.log("error calling axios", error);
+        })
+
     }
+
+    const handleCancelClick = (e) => {
+        window.location.replace("/");
+    }
+
+
     return (
         <>
             <section className='upload'>
                 <h1 className='upload__h1'>Upload Video</h1>
-                <div className='upload__content'>
-                <article className='upload__section'>
-                <h2 className='upload__h2'>VIDEO THUMBNAIL</h2>
-                <img className='upload__img' src={uploadImg} alt=''></img>
-                </article>
-                <article className='upload__section'>
-                <h2 className='upload__h2'>TITLE YOUR VIDEO</h2>
-                <textarea
-                    className='upload__title'
-                    name='uploadTitle'
-                    placeholder='Add a title to your video'
-                    required
-                    rows='1'
-                ></textarea>
-                <h2 className='upload__h2'>ADD A DESCRIPTION</h2>
-                <textarea
-                    className='upload__description'
-                    name='uploadDescription'
-                    placeholder='Add a description to your video'
-                    required
-                    rows='5'></textarea>
-                </article>
-                </div>
-                <div className='upload__bottom'>
-                <Link className='upload__link'  to={`/`}><button className='upload__send' type='upload' onClick={handleSubmitClick}><img src={publishIcon} alt='' className='upload__publish-img'></img>PUBLISH</button></Link>
-                <button className='upload__cancel' type='cancel'>CANCEL</button>
-                </div>
+                <form onSubmit={handleSubmitClick}>
+                    <div className='upload__content'>
+                        <article className='upload__section'>
+                            <h2 className='upload__h2'>VIDEO THUMBNAIL</h2>
+                            <img className='upload__img' src='http://localhost:8000/images/140555ee-1481-4501-b0d1-f0775637a8dd' alt=''></img>
+                        </article>
+                        <article className='upload__section'>
+                            <h2 className='upload__h2'>TITLE YOUR VIDEO</h2>
+                            <textarea
+                                className='upload__title'
+                                name='uploadTitle'
+                                placeholder='Add a title to your video'
+                                value={uploadTitle}
+                                onChange={handleTitleChange}
+                                required
+                                rows='1'
+                            ></textarea>
+                            <h2 className='upload__h2'>ADD A DESCRIPTION</h2>
+                            <textarea
+                                className='upload__description'
+                                name='uploadDescription'
+                                placeholder='Add a description to your video'
+                                value={uploadDescription}
+                                onChange={handleDescriptionChange}
+                                required
+                                rows='5'></textarea>
+                        </article>
+                    </div>
+                    <div className='upload__bottom'>
+                        <button className='upload__send' type='submit'><img src={publishIcon} alt='' className='upload__publish-img'></img>PUBLISH</button>
+                        <button className='upload__cancel' onClick={handleCancelClick}>CANCEL</button>
+                    </div>
+                </form>
             </section>
         </>
     );
